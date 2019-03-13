10b0f4089e922d6d7309eda50d197837468d461b
txazo <784990655@qq.com>
2019-03-13 10:12:59 +0800

    update

--- src/main/java/com/lmax/disruptor/EventProcessor.java
+++ src/main/java/com/lmax/disruptor/EventProcessor.java
@@ -23,6 +23,8 @@ package com.lmax.disruptor;
  * <p>
  * An EventProcessor will generally be associated with a Thread for execution.
  */
+
+// 事件执行器, 等待RingBuffer有可用消费事件, 一个事件处理器关联一个执行线程
 public interface EventProcessor extends Runnable
 {
     /**
